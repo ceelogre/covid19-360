8ea40e6e300cfb85f505a76bfbeb246fbf13d720
ceelogre <sam.nishimwe@yahoo.com>
2020-07-09 15:24:05 +0200

    Ch: Add gitignore and readme files
    ch: Update dependencies
    
    ch: Remove node modules
    
    ch: Add the source dir

---
+++ README.md
@@ -0,0 +1,1 @@
+#Covid data for Africa
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ class App extends React.Component {
+     render () {
+         return (
+             <div>
+             <h1>COVID</h1>
+             </div>
+         )
+     }
+ }
